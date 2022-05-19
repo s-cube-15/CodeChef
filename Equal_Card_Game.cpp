d29104dd7ba3b96866d924961a797811cc328a3a
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2022-05-19 08:22:50 +0530

    Create Equal_Card_Game.cpp

---
+++ Equal_Card_Game.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, a, b, c;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a;
+        b = (52 % a);
+        cout << b << endl; /* code */
+    }
+
+    return 0;
+}
