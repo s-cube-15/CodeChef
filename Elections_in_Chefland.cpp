5a3da42c666eaa792570a4d051a08937d7cfd76b
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2022-05-19 08:22:42 +0530

    Create Elections_in_Chefland.cpp

---
+++ Elections_in_Chefland.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, a, b, c;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a >> b >> c;
+        if (a > 50)
+        {
+            cout << "A" << endl;
+            /* code */
+        }
+
+        else if (b > 50)
+        {
+            cout << "B" << endl;
+            /* code */
+        }
+
+        else if (c > 50)
+        {
+            cout << "C" << endl;
+            /* code */
+        }
+        else
+            cout << "NOTA" << endl;
+    }
+
+    return 0;
+}
