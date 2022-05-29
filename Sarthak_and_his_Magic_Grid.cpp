d2626ac758a8f821316f116220a4e67cb0dafe7e
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2022-05-29 23:47:41 +0530

    Create Sarthak_and_his_Magic_Grid.cpp

---
+++ Sarthak_and_his_Magic_Grid.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,a,b,c;
+    cin>>t;
+    while (t--)
+    {
+        cin>>a;
+        for (int i = 0; i < a; i++)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                cout<<"1 ";
+            }cout<<endl;
+            
+        }
+        
+    }
+    
+    return 0;
+}
