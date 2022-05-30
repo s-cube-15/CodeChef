c23348d1122b5c90be3b7f73b5583590f9edabe1
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2022-05-31 00:04:29 +0530

    Create Prefix_Permutation.cpp

---
+++ Prefix_Permutation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, a, b, c;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a >> b;
+        int q[b];
+        for (int i = 0; i < b; i++)
+        {
+            /* code */ cin >> q[i];
+        }
+        for (int i = 1; i <= a; i++)
+        {
+            /* code */ cout << i<<" ";
+        }
+        cout << endl;
+
+        /* code */
+    }
+
+    return 0;
+}
